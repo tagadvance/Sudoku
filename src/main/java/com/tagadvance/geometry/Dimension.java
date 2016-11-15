ac8b969b8318674705e4820d0fe1838acc315223
Tag <tagadvance@gmail.com>
2016-11-15 03:30:36 -0800

    override hashCode, equals, and toString methods

--- src/main/java/com/tagadvance/geometry/Dimension.java
+++ src/main/java/com/tagadvance/geometry/Dimension.java
@@ -1,5 +1,7 @@
 package com.tagadvance.geometry;
 
+import com.google.common.base.MoreObjects;
+
 public class Dimension implements ImmutableDimension {
 
 	private final int width, height;
@@ -18,6 +20,39 @@ public class Dimension implements ImmutableDimension {
 	@Override
 	public int getHeight() {
 		return this.height;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + width;
+		result = prime * result + height;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		} else if (obj == null) {
+			return false;
+		} else if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Dimension other = (Dimension) obj;
+		if (width != other.width) {
+			return false;
+		} else if (height != other.height) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return MoreObjects.toStringHelper(Dimension.class).add("width", width).add("height", height)
+				.toString();
 	}
 
 }
--- src/main/java/com/tagadvance/geometry/Point.java
+++ src/main/java/com/tagadvance/geometry/Point.java
@@ -1,5 +1,7 @@
 package com.tagadvance.geometry;
 
+import com.google.common.base.MoreObjects;
+
 public class Point implements ImmutablePoint {
 
 	private final int x, y;
@@ -18,6 +20,38 @@ public class Point implements ImmutablePoint {
 	@Override
 	public int getY() {
 		return this.y;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		} else if (obj == null) {
+			return false;
+		} else if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Point other = (Point) obj;
+		if (x != other.x) {
+			return false;
+		} else if (y != other.y) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return MoreObjects.toStringHelper(Point.class).add("x", x).add("y", y).toString();
 	}
 
 }
